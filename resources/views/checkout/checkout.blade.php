e043d23338fb6426abe1aa25f68f5e2a05779d41
Cyprian Kyalo <cypkyalo17@gmail.com>
2022-07-14 15:56:40 +0300

    Update Quantity

--- resources/views/checkout/checkout.blade.php
+++ resources/views/checkout/checkout.blade.php
@@ -8,10 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="" />
     <meta name="keywords" content="" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Swyft2eat</title>
     <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
 
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.5/jquery.timepicker.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css"> --}}
@@ -223,7 +225,7 @@
 			                {{-- <input type="hidden" name="prod_id[]" value=" {{ $details['prod_id'] }} " id="product_id">
 			                <input type="hidden" name="prod_qty[]" value=" {{ $details['quantity'] }} ">
 			                <input type="hidden" name="user_id" value="{{Auth::user()->id}}"> --}}
-			                <tr>
+			                <tr data-id="{{ $details['prod_id'] }}">
 			                    <td data-th="Product">
 			                        <div class="row">
 			                            <div class="col-sm-3 hidden-xs"><img src="/images/product/{{ $details['photo'] }}" width="100px" height="100px" class="img-responsive"/></div>
@@ -239,8 +241,12 @@
 
 			                    <td data-th="Price" id="price">Kshs.{{ $details['price'] }}</td>
 			                    <td data-th="Quantity" class="text-center">
-                                    <input type="number" min="1" id={{ "quant".$details['prod_id'] }} oninput="myFunction({{ $details['price'] }}, {{ $details['prod_id'] }})" value="1" style="width:60%" name="quantity">
+                                    <input type="number" min="1" id={{ "quant".$details['prod_id'] }} oninput="myFunction({{ $details['price'] }}, {{ $details['prod_id'] }})" value="1" style="width:60%" name="quantity" class="update-cart qty">
 			                    </td>
+                                {{-- <td>
+                                    <button class="btn btn-info btn-sm update-cart" data-token="{{ csrf_token() }}" data-id="{{ $details['prod_id']}}">Update Cart</button>
+                                </td> --}}
+
                                 {{-- <td id="tot"></td> --}}
 			                    <td data-th="Subtotal" class="text-center">Kshs.<span class="product-subtotal" id={{ "total".$details['prod_id'] }}>{{ $details['price'] }}</span></td>
 
@@ -385,6 +391,40 @@
         function updatePrice(quantity, standardPrice,productId){
             document.getElementById('total'+productId).innerHTML = quantity*standardPrice;
         }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $(".update-cart").change(function (e) {
+            e.preventDefault();
+            // console.log = 'hihs';
+            // alert('Yesyes');
+            var ele = $(this);
+            // var ele = $(this);
+            $.ajax({
+                url: "{{ route('update.cart') }}",
+                method: "patch",
+                dataType: 'json',
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    id: 1,
+                    quantity: 30
+                },
+                // alert('data');
+                success: function (data) {
+                    console.log('Success: ', data);
+                    // window.location.reload();
+                },
+
+                error: function(data) {
+                    // console.log('Error: ', thrownError);
+                    console.log('Error: ', data);
+                }
+            });
+        });
 
         // $(function()) {
         //     $('#timepicker').on('click', function(e) {
